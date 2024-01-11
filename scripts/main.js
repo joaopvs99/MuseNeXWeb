d8c9a09e0b14e530edc3f9d0934835e01a928ca5
joaopvs99 <jpvianasoares@gmail.com>
2024-01-11 01:51:10 +0000

    sidebar updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,15 @@
-(function($) {
-
-	"use strict";
-
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	$('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
-  });
-
-})(jQuery);
+$(function(){
+    // toggle sidebar collapse
+    $('.btn-toggle-sidebar').on('click', function(){
+        $('.wrapper').toggleClass('sidebar-collapse');
+    });
+    // mark sidebar item as active when clicked
+    $('.sb-item').on('click', function(){
+        if ($(this).hasClass('btn-toggle-sidebar')) {
+          return; // already actived
+        }
+        $(this).siblings().removeClass('active');
+        $(this).siblings().find('.sb-item').removeClass('active');
+        $(this).addClass('active');
+    })
+});
