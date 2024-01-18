7ed926f7a5475405659ef9ee555ff69291c42250
joaopvs99 <jpvianasoares@gmail.com>
2024-01-18 15:20:23 +0000

    conteudo da main e scripts de reposição do innerhtml

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,38 @@ $(function(){
     $('.btn-toggle-sidebar').on('click', function(){
         $('.wrapper').toggleClass('sidebar-collapse');
     });
-    // mark sidebar item as active when clicked
     $('.sb-item').on('click', function(){
         if ($(this).hasClass('btn-toggle-sidebar')) {
-          return; // already actived
+          return; 
         }
         $(this).siblings().removeClass('active');
         $(this).siblings().find('.sb-item').removeClass('active');
         $(this).addClass('active');
     })
 });
+
+document.getElementById('header').addEventListener('click', function() {
+    window.location.href = '../index.html';
+});
+
+
+$(document).ready(function () {
+    $(".sidebar-buttons").on("click", function () {
+
+        var pageId = $(this).data("page-id");
+
+        nextpage(pageId);
+    });
+});
+
+function nextpage(idd) {
+    $.get("homecontent/page" + idd + ".html")
+        .done(function (data) {
+            $("#pageContent").html(data);
+        })
+        .fail(function (error) {
+            console.error("Error fetching page:", error);
+        });
+}
+
+
